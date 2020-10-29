799bbf5d1afb5e374c1d1280e99d195da9dadd7b
Jonathon Wright <jwright@atlassian.com>
2020-10-29 18:27:29 -0500

    changes

--- app.py
+++ app.py
@@ -5,26 +5,30 @@ from app_blueprint import app_blueprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite::///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20),
+                           nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     payment_profile = db.Column(db.String(60))
     major = db.Column(db.String(20))
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('Posts', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
+
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     isbn = db.Column(db.String(120), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
     title = db.Column(db.Text, nullable=False)
     publisher = db.Column(db.Text, nullable=False)
     writers = db.Column(db.Text, nullable=False)
@@ -33,9 +37,9 @@ class Posts(db.Model):
     price = db.Column(db.String(60), nullable=False)
     major = db.Column(db.String(20))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
+
     def __repr__(self):
         return f"Post('{self.title},')"
 
-app.register_blueprint(app_blueprint)
 
+app.register_blueprint(app_blueprint)
