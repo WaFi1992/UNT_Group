e8645209b33565c8072b3c01f55c38a0b20e017a
Jonathon Wright <jonathonwright@my.unt.edu>
2020-10-31 16:43:27 -0700

    This is what I have done so far, this includes the database. We can make accounts, log in, and almost make posts, we're looking really good

--- bookapp/forms.py
+++ bookapp/forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from bookapp.models import User
 
 
 class RegistrationForm(FlaskForm):
     
-    def validateEmail(form, field):
+    def validateEmailDomain(form, field):
         if 'unt.edu' not in field.data:
             raise ValidationError('Email must be from a UNT address')
 
-
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email(check_deliverability=True), validateEmail])
+    email = StringField('Email', validators=[DataRequired(), Email(check_deliverability=True), validateEmailDomain])
     password = PasswordField('Password',
                              validators=[DataRequired(), Length(min=5)])
     confirm_password = PasswordField('Confirm Password',
@@ -22,6 +22,17 @@ class RegistrationForm(FlaskForm):
     major = StringField('Major',
                            validators=[Length(min=2, max=20)])
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is taken')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('An account with that email already exists')               
+
 
 
 class LoginForm(FlaskForm):
