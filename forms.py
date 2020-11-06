a6a764ef0e83142a5e184dd8461f3d22e0cfd577
TheNamesGimp <44327058+TheNamesGimp@users.noreply.github.com>
2020-11-05 22:11:23 -0600

    rough version of scrape in postform

--- forms.py
+++ forms.py
@@ -2,6 +2,23 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
+import requests
+from bs4 import BeautifulSoup
+import re
+
+#pass URL into scrape function
+def scrape(url):
+
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    #find elements
+    imgCover = soup.find(id='coverImage')
+    title = soup.find(id='describe-isbn-title')
+    publisher = soup.find(itemprop='publisher')
+    author = soup.find(itemprop='author')
+
 
 class RegistrationForm(FlaskForm):
     
@@ -33,7 +50,32 @@ class LoginForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    isbn = StringField('ISBN', validators=[DataRequired()])
+    #isbn = StringField('ISBN', validators=[DataRequired()])
+
+    isbn = 9781947556744
+
+    #(scraping) insert isbn into url to navigate to page with book info
+    url = f"https://www.bookfinder.com/search/?author=&title=&lang=en&isbn{str(isbn)}&new_used=*&destination=us&currency=USD&mode=basic&st=sr&ac=qr"
+    
+    
+
+    #scraping
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    #(scraping) find elements
+    #might have to take another look at how to retrieve the img
+    imgCover = soup.find(id='coverImage')
+    
+    #find title in between brackets
+    title = soup.find(id='describe-isbn-title')
+    #xTitle = re.search(r">(.*)<", title.prettify())
+    
+
+    publisher = soup.find(itemprop='publisher')
+    author = soup.find(itemprop='author')
+
+    #data required from user
     condition = StringField('Condition', validators=[DataRequired()])
     price = StringField('Price', validators=[DataRequired()])
     major = StringField('Major', validators=[DataRequired()])
