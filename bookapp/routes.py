ad432a0d91dae361abd66082749a5852a49ea630
Jonathon Wright <jonathonwright@my.unt.edu>
2020-11-13 19:18:43 -0800

    added update account page

--- bookapp/routes.py
+++ bookapp/routes.py
@@ -1,6 +1,10 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import login_user, current_user, logout_user, login_required
-from bookapp.forms import RegistrationForm, LoginForm, PostForm
+from bookapp.forms import RegistrationForm, LoginForm, PostForm, UpdateAccountForm
 from bookapp.models import User, Posts
 from bookapp import app, db, bcrypt
 
@@ -67,10 +71,41 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route('/account', methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template('account.html', title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.major = form.major.data
+        current_user.payment_profile = form.payment_profile.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.major.data = current_user.major
+        form.payment_profile.data = current_user.payment_profile
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title="Account", image_file=image_file, form=form)
 
 @app.route('/post/new', methods=["GET", "POST"])
 @login_required
