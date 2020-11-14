d09f0411c6193db547045b9218f56d685919a656
Jonathon Wright <jonathonwright@my.unt.edu>
2020-11-13 19:18:08 -0800

    added update account form

--- bookapp/forms.py
+++ bookapp/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from bookapp.models import User
@@ -41,6 +43,36 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired(), Length(min=5)])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign in')
+
+class UpdateAccountForm(FlaskForm):
+
+
+    def validateEmailDomain(form, field):
+        if 'unt.edu' not in field.data:
+            raise ValidationError('Email must be from a UNT address')
+
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(check_deliverability=True), validateEmailDomain])
+    payment_profile = StringField('Payment Profile',
+                           validators=[Length(min=2, max=25)])
+    major = StringField('Major',
+                           validators=[Length(min=2, max=20)])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
 
 
 class PostForm(FlaskForm):
