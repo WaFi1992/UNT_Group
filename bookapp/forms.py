47b1042bc64256aab1c226d600dcddf83a10526b
WaFi1992 <66385207+WaFi1992@users.noreply.github.com>
2020-11-15 20:25:10 -0600

    Email and Password Reset
    Added code and two new files; reset_request and reset_token so that the Email and Password Reset functions are made.

--- bookapp/forms.py
+++ bookapp/forms.py
@@ -61,7 +61,22 @@ class PostForm(FlaskForm):
     major = StringField('Major', validators=[DataRequired()])
     submit = SubmitField('Post Book')
     
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Request Password Reset')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')    
 
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=5)])
+    confirm_password = PasswordField('Confirm Password',
+                             validators=[DataRequired(), Length(min=5), EqualTo('password')])
+     submit = SubmitField('Reset Password')                    
+    
 class CommentForm(FlaskForm):
     comment = StringField('Comment', validators=[DataRequired()])
     submit = SubmitField('Post Comment')
--- bookapp/models.py
+++ bookapp/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from bookapp import db, loginManager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from bookapp import db, loginManager, app
 from flask_login import UserMixin
 
 @loginManager.user_loader
@@ -16,6 +17,19 @@ class User(db.Model, UserMixin):
     payment_profile = db.Column(db.String(60))
     major = db.Column(db.String(20))
     posts = db.relationship('Posts', backref='author', lazy=True)
+    
+    def get_reset_token(self, expire_sec=1800)
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+        
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
